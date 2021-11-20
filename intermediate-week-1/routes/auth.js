e08565226bdc514f538c83496c38b5cac7110d4a
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-11-20 19:37:24 +0100

    feat: add tasks for refresh token

--- intermediate-week-1/routes/auth.js
+++ intermediate-week-1/routes/auth.js
@@ -1,9 +1,11 @@
 const login = require('../controllers/auth/login');
+const refreshToken = require('../controllers/auth/refresh-token');
 const signup = require('../controllers/auth/signup');
 
 const router = require('express').Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
+router.post('/refresh-token', refreshToken)
 
 module.exports = router;
